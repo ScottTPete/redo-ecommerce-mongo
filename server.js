0ac9e496b598d7394a998401652e8c9db18edee3
Scott Peterson <stpeterson28@yahoo.com>
2016-04-25 17:22:56 -0600

    Server set up. Need to create the front end.

---
+++ server.js
@@ -0,0 +1,95 @@
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	cors = require('cors'),
+	mongojs = require('mongojs'),
+	app = express()
+
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+//Database configuration//
+var db = mongojs('ecommerceTwo', ['products']),
+	productId = mongojs.ObjectId;
+
+db.on('error', function (err) {
+	console.log('database error', err)
+})
+
+db.on('connect', function () {
+	console.log('database connected')
+})
+
+app.post('/api/products', function(req, res, next) {
+	db.products.save(req.body, function(err, response) {
+		if(err) {
+			res.status(500).send(err)
+		} else {
+			res.status(200).json(response);
+		}
+	})
+})
+
+app.get('/api/products', function(req, res, next) {
+	var query = req.query //is an empty object if nothing passed in
+	console.log(query)
+	db.products.find(query, function(err, response) {  //find with an empty object returns entire collection
+		if(err) {
+			res.status(500).send(err)
+		} else {
+			res.status(200).json(response);
+		}
+	})
+})
+
+app.get('/api/products/:id', function(req, res, next) {
+	db.products.find({_id: productId(req.params.id)}, function(err, response) {
+		if(err) {
+			res.status(500).send(err)
+		} else {
+			res.status(200).json(response);
+		}
+	})
+})
+
+app.put('/api/products/:id', function(req, res, next) {
+	db.products.update({_id: productId(req.params.id)}, {$set: req.body}, function(err, response) {
+		if(err) {
+			res.status(500).send(err)
+		} else {
+			res.status(200).json(response);
+		}
+	})
+	
+})
+
+app.delete('/api/products/:id', function(req, res, next) {
+	db.products.remove({_id: productId(req.params.id)}, function(err, response) {
+		if(err) {
+			res.status(500).send(err)
+		} else {
+			res.status(200).json(response);
+		}
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//SET UP PORT//
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+	console.log('Listening on port ' + port);
+})
